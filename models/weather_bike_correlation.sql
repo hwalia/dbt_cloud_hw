1472219bf8a28a330183596d30083712550dd4f0
Hemant.walia1988@gmail.com <Hemant.walia1988@gmail.com>
2025-12-15 19:59:41 +0000

    committing fixes for correlation.

--- models/weather_bike_correlation.sql
+++ models/weather_bike_correlation.sql
@@ -1,9 +1,9 @@
 WITH CTE AS (
     select t.*
         ,w.*
-    from {{ ref('trip_fact') }} t
+    from {{ ref('fact_trip') }} t
     left join {{ ref('daily_weather') }} w
-    on t.TRIP_DATE = w.DAILY_WEATHER
+    on t.TRIP_DATE = w.RECORD_DATE
 )
 select *
 from CTE
